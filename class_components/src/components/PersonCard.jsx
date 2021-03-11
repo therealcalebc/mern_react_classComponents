2b2a4d09ab1d34de19969d665b2afb89808359d7
Caleb A Comstock <c_comstock@rocketmail.com>
2021-03-10 22:43:43 -0800

    updated the PersonCard component to include a button that increments age

--- class_components/src/components/PersonCard.jsx
+++ class_components/src/components/PersonCard.jsx
@@ -2,18 +2,32 @@ import React, { Component } from "react";
 import "./PersonCard.css";
 
 class PersonCard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			age: this.props.age,
+		};
+	}
 	render() {
-		const { firstName, lastName, age, hairColor } = this.props;
+		const { firstName, lastName, hairColor } = this.props;
 		return (
 			<div className='PersonCard'>
 				<h2>
 					{lastName}, {firstName}
 				</h2>
-				<p>Age: {age}</p>
+				<p>Age: {this.state.age}</p>
 				<p>Hair Color: {hairColor}</p>
+				<button onClick={this.incrementAge}>
+					Birthday Button for {firstName} {lastName}
+				</button>
 			</div>
 		);
 	}
+	incrementAge = () => {
+		let temp = this.state.age;
+		temp++;
+		this.setState({ age: temp });
+	};
 }
 
 export default PersonCard;
