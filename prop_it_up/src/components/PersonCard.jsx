9271c66908a0a8cf8facfe52e901e83cbfcd1bb7
Caleb A Comstock <c_comstock@rocketmail.com>
2021-03-10 18:06:59 -0800

    added project files

---
+++ prop_it_up/src/components/PersonCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./PersonCard.css";
+
+class PersonCard extends Component {
+	render() {
+		const { firstName, lastName, age, hairColor } = this.props;
+		return (
+			<div className='PersonCard'>
+				<h2>
+					{lastName}, {firstName}
+				</h2>
+				<p>Age: {age}</p>
+				<p>Hair Color: {hairColor}</p>
+			</div>
+		);
+	}
+}
+
+export default PersonCard;
