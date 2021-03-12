e3d7fb044c9cedf3ee7ff375127423bf3e7f963f
Caleb A Comstock <c_comstock@rocketmail.com>
2021-03-11 17:33:04 -0800

    converted the PersonCard  from class component to functional component

--- people/src/components/PersonCard.jsx
+++ people/src/components/PersonCard.jsx
@@ -1,31 +1,45 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 import "./PersonCard.css";
 
-class PersonCard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			age: this.props.age,
-		};
-	}
-	render() {
-		const { firstName, lastName, hairColor } = this.props;
-		return (
-			<div className='PersonCard'>
-				<h2>
-					{lastName}, {firstName}
-				</h2>
-				<p>Age: {this.state.age}</p>
-				<p>Hair Color: {hairColor}</p>
-				<button onClick={this.incrementAge}>
-					Birthday Button for {firstName} {lastName}
-				</button>
-			</div>
-		);
-	}
-	incrementAge = () => {
-		this.setState({ age: this.state.age + 1 });
-	};
-}
+const PersonCard = (props) => {
+	let { firstName, lastName, age, hairColor } = props;
+	return (
+		<div className='PersonCard'>
+			<h2>
+				{lastName}, {firstName}
+			</h2>
+			<p>Age: {age}</p>
+			<p>Hair Color: {hairColor}</p>
+		</div>
+	);
+};
+
+// class PersonCard extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			age: this.props.age,
+// 		};
+// 	}
+// 	render() {
+// 		const { firstName, lastName, hairColor } = this.props;
+// 		return (
+// 			<div className='PersonCard'>
+// 				<h2>
+// 					{lastName}, {firstName}
+// 				</h2>
+// 				<p>Age: {this.state.age}</p>
+// 				<p>Hair Color: {hairColor}</p>
+// 				<button onClick={this.incrementAge}>
+// 					Birthday Button for {firstName} {lastName}
+// 				</button>
+// 			</div>
+// 		);
+// 	}
+// 	incrementAge = () => {
+// 		this.setState({ age: this.state.age + 1 });
+// 	};
+// }
 
 export default PersonCard;
