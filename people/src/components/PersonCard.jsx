01ae2814deb8c068beb8ff3c3eba035565e5d8f0
Caleb A Comstock <c_comstock@rocketmail.com>
2021-03-11 17:44:28 -0800

    updated PersonCard to re-add the birthday button & clean up the code

--- people/src/components/PersonCard.jsx
+++ people/src/components/PersonCard.jsx
@@ -1,9 +1,12 @@
-// import React, { Component } from "react";
-import React from "react";
+import React, { useState } from "react";
 import "./PersonCard.css";
 
 const PersonCard = (props) => {
-	let { firstName, lastName, age, hairColor } = props;
+	const { firstName, lastName, hairColor } = props;
+	const [age, setAge] = useState(props.age);
+	const incrementAge = () => {
+		setAge(age + 1);
+	};
 	return (
 		<div className='PersonCard'>
 			<h2>
@@ -11,35 +14,11 @@ const PersonCard = (props) => {
 			</h2>
 			<p>Age: {age}</p>
 			<p>Hair Color: {hairColor}</p>
+			<button onClick={incrementAge}>
+				Birthday Button for {firstName} {lastName}
+			</button>
 		</div>
 	);
 };
-
-// class PersonCard extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			age: this.props.age,
-// 		};
-// 	}
-// 	render() {
-// 		const { firstName, lastName, hairColor } = this.props;
-// 		return (
-// 			<div className='PersonCard'>
-// 				<h2>
-// 					{lastName}, {firstName}
-// 				</h2>
-// 				<p>Age: {this.state.age}</p>
-// 				<p>Hair Color: {hairColor}</p>
-// 				<button onClick={this.incrementAge}>
-// 					Birthday Button for {firstName} {lastName}
-// 				</button>
-// 			</div>
-// 		);
-// 	}
-// 	incrementAge = () => {
-// 		this.setState({ age: this.state.age + 1 });
-// 	};
-// }
 
 export default PersonCard;
